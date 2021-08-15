d498bc553b674ab109e94ea04834013305646c60
thepresidentm <miguelanva9@gmail.com>
2021-08-15 13:07:42 -0500

    Web page

---
+++ web page/frameChanger.js
@@ -0,0 +1,21 @@
+var totalFrames, actualFrame, container, nextFrame, fileName, fileExtension, actualFile;
+totalFrames = 7520;
+fileExtension = ".html";
+time = 400;
+steps = 11;
+
+function change(){
+    container = document.getElementById("content");
+    actualFile = container.getAttribute("src");
+    actualFrame = actualFile.split(".");
+    if(actualFrame[0] >= totalFrames){
+        alert("end")
+    } else{
+        nextFrame = parseInt(actualFrame[0]) + steps;
+        fileName = nextFrame + fileExtension; 
+        container.setAttribute("src", fileName);
+    }
+}
+function delay(){
+    setTimeout(change, time);
+}
